e3908d613d2a128af09100b578eb07f05229dd9f
anmol garg <156640161+garganmol0@users.noreply.github.com>
2026-05-20 17:41:58 +0530

    Change server port from 8000 to 5000

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,7 @@ app.use('/api/projects', projectRoutes)
 app.use('/api/tasks', taskRoutes)
 app.use('/api/activity', activityRoutes)
 app.use(errorHandler)
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 5000
 mongoose.connect(process.env.MONGO_URI).then(()=>{
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
